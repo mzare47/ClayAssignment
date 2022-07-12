038de85bc38a3c507c8964ba5414747c4ad217ec
mzare47 <majid.zare.47@gmail.com>
2022-07-12 22:40:30 +0430

    add AuthenticateController. Add IAuditable and BaseEntity for Setup Audit process when SaveChangesAsync call. Add ClayDbContext

---
+++ Services/AccessControl.Api/Data/ClayDbContext.cs
@@ -0,0 +1,24 @@
+﻿using AccessControl.Api.Models.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccessControl.Api.Data
+{
+    public class ClayDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ClayDbContext(DbContextOptions<ClayDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+    }
+}
---
+++ Services/AccessControl.Api/Models/Entity/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using AccessControl.Api.Models.Shared;
+using Microsoft.AspNetCore.Identity;
+
+namespace AccessControl.Api.Models.Entity
+{
+    public class ApplicationUser : IdentityUser, IAuditable
+    {
+        public DateTime? CreatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
