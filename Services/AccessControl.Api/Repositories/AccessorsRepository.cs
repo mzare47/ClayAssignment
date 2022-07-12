207b168d0015d0abaa604a2d176ebcedbd340e63
mzare47 <majid.zare.47@gmail.com>
2022-07-13 01:16:21 +0430

    Create AccessorsRepository and Add it to program services

---
+++ Services/AccessControl.Api/Repositories/AccessorsRepository.cs
@@ -0,0 +1,40 @@
+﻿using AccessControl.Api.Data;
+using AccessControl.Api.Models.Entity;
+using AccessControl.Api.Repositories.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Shared.Lib.Models;
+
+namespace AccessControl.Api.Repositories
+{
+    public class AccessorsRepository : IAccessorsRepository
+    {
+        protected readonly ClayDbContext _dbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccessorsRepository(ClayDbContext dbContext, UserManager<ApplicationUser> userManager)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        }
+
+        public async Task<ApplicationUser> GetAccessorByIdAsync(string accessorId)
+        {
+            return await _dbContext.Users
+                                .AsNoTracking()
+                                .FirstAsync(u => u.Id.Equals(accessorId));
+        }
+
+        public async Task<IEnumerable<ApplicationUser>> GetAllAccessors()
+        {
+            return await _userManager.GetUsersInRoleAsync(UserRole.Accessor);
+        }
+
+        public async Task DeleteAccessorAsync(ApplicationUser accessor)
+        {
+            _dbContext.UserRoles.RemoveRange(_dbContext.UserRoles.Where(u => u.UserId.Equals(accessor.Id)));
+            _dbContext.Users.Remove(accessor);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Services/AccessControl.Api/Repositories/Interfaces/IAccessorsRepository.cs
@@ -0,0 +1,11 @@
+﻿using AccessControl.Api.Models.Entity;
+
+namespace AccessControl.Api.Repositories.Interfaces
+{
+    public interface IAccessorsRepository
+    {
+        Task<IEnumerable<ApplicationUser>> GetAllAccessors();
+        Task<ApplicationUser> GetAccessorByIdAsync(string id);
+        Task DeleteAccessorAsync(ApplicationUser accessor);
+    }
+}
