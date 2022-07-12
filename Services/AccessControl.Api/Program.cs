8aaa7d37826d423aa4e4bcb951359bba7a0da721
mzare47 <majid.zare.47@gmail.com>
2022-07-12 23:50:51 +0430

    Refactor appsettings and Program.cs for Confuring Dd and Jwt

--- Services/AccessControl.Api/Program.cs
+++ Services/AccessControl.Api/Program.cs
@@ -1,8 +1,63 @@
+using AccessControl.Api.Data;
+using AccessControl.Api.Models.Entity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+// For Entity Framework
+if (configuration["Settings:UseInMemoryDb"] == "true")
+{
+    //InMemory Db Conf
+    builder.Services.AddDbContext<ClayDbContext>(options =>
+                    options.UseInMemoryDatabase(databaseName: "ClayDb"));
+}
+else
+{
+    //SQL DB Conf
+    builder.Services.AddDbContext<ClayDbContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("ClayDbConnectionString")));
+}
+
+// For Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ClayDbContext>()
+    .AddDefaultTokenProviders();
+
+// Adding Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+
+// Adding Jwt Bearer
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = configuration["JWT:ValidAudience"],
+        ValidIssuer = configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+    };
+});
+
+// Add Controllers Config
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
