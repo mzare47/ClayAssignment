e99ccf4635f9983261c34a5592ec367c2d57beec
mzare47 <majid.zare.47@gmail.com>
2022-07-13 08:02:44 +0430

    Add IntegrationTests project and set configs. Config Api for integaration testing

---
+++ Tests/IntegrationTests/Factories/AccessControlApiApplicationFactory.cs
@@ -0,0 +1,28 @@
+﻿using AccessControl.Api.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IntegrationTests.Factories
+{
+    public class AccessControlApiApplicationFactory : WebApplicationFactory<AccessControlApiProgram>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbContextOptions<ClayDbContext>));
+
+                services.Remove(descriptor);
+
+                services.AddDbContext<ClayDbContext>(options =>
+                            options.UseInMemoryDatabase(databaseName: "ClayDbTest"));
+            });
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
